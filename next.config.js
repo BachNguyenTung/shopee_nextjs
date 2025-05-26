988d750d564f68c00ff9655a35582bd1f287d9e5
tungbach <nguyentungbach1992@gmail.com>
2025-05-26 20:38:06 +0700

    fix(XSS,CSRF): add auth stripe domain + remove unused, remove unused

--- next.config.js
+++ next.config.js
@@ -2,17 +2,25 @@
 
 const cspHeader = `
     default-src 'self';
-    connect-src 'self' https://identitytoolkit.googleapis.com https://firestore.googleapis.com https://us-central1-shopee-demo-c6d2b.cloudfunctions.net https://firebasestorage.googleapis.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline' localhost;
-    img-src 'self' blob: data: res.cloudinary.com fakestoreapi.com;
+    connect-src 'self'
+        https://*.googleapis.com
+        https://us-central1-shopee-demo-c6d2b.cloudfunctions.net
+        https://*.stripe.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline'
+        https://*.stripe.com;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:
+        res.cloudinary.com
+        fakestoreapi.com;
     font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
+    frame-src
+        https://*.stripe.com;
     frame-ancestors 'none';
     upgrade-insecure-requests;
-`
+`;
 
 const nextConfig = {
   reactStrictMode: true,
@@ -26,8 +34,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, '')
           },
           {
             key: "Set-Cookie",
