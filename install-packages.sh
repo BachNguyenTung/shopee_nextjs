ab324322b307876b159a0cb6aac4182a5a23af82
tungbach <nguyentungbach1992@gmail.com>
2025-06-09 05:05:25 +0700

    feat(vercel): client side exception

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Installing packages in monorepo"
 cd packages/utils && npm install
-cd ../ui && npm install
-cd ../tailwind-config && npm install
+cd packages/ui && npm install
+cd packages/tailwind-config && npm install
+cd packages/eslint-config && npm install
 cd ../..
