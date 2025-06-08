4ed736ba867cae5c9ae46ad5b919b316cf350feb
tungbach <nguyentungbach1992@gmail.com>
2025-06-08 23:42:59 +0700

    feat(vercel deploy): set up

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://asia-east1-shopee-demo-c6d2b.cloudfunctions.net/api";
 //http://localhost:5001/shopee-demo-c6d2b/us-central1/api
 //https://us-central1-shopee-demo-c6d2b.cloudfunctions.net/api
 
 const instance = axios.create({
-  baseURL: BASE_URL, // api url(our cloud func)
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // api url(our cloud func)
   cancelToken: axios.CancelToken.source().token,
 });
 
