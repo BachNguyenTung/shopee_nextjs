659a9d0f739420ac92861353b93e5a94ea5a77aa
tungbach <nguyentungbach1992@gmail.com>
2025-06-11 12:56:07 +0700

    fix: no need install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-echo "Installing packages in monorepo"
-cd packages/utils && npm install
-cd packages/ui && npm install
-cd packages/tailwind-config && npm install
-cd packages/eslint-config && npm install
-cd ../..
+##!/bin/bash
+#echo "Installing packages in monorepo"
+#cd packages/utils && npm install
+#cd ../ui && npm install
+#cd ../tailwind-config && npm install
+#cd ../eslint-config && npm install
+#cd ../..
