5f3a674721aed70ac2c4e8d34af25758945c6fc3
tungbach <nguyentungbach1992@gmail.com>
2025-06-11 12:43:44 +0700

    ref: meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
         <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
+        {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />*/}
         <meta name="author" content="Shopee" />
         <meta name="generator" content="Shopee E-commerce" />
         <meta name="copyright" content="Shopee © 2025" />
