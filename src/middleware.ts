dfd971e9a98529ca0222da18efb515f11ae9acaa
tungbach <nguyentungbach1992@gmail.com>
2025-07-02 18:04:29 +0700

    feat: matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,13 +45,13 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-    * Match all request paths except for the ones starting with:
-    * - api (API routes)
-    * - _next/static (static files)
-    * - _next/image (image optimization files)
-    * - img/ (img/ directory for images)
-    * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-    */
-    '/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest|img/|sitemap.xml|robots.txt).*)',
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - manifest.webmanifest, images/, img/ (your static asset folders)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest|images/|img/|sitemap.xml|robots.txt).*)',
   ],
 }
